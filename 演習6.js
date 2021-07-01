c34f0a17221632ccaea9eda70c2bae88833ec87d
kawaabtamayu <2195032@stu.hus.ac.jp>
2021-07-01 23:54:13 +0900

    演習6

---
+++ 演習6.js
@@ -0,0 +1,19 @@
+for(let i=1; i<=30; i++){
+   
+    if(i%3 === 0){ 
+        if(i%5 === 0){
+            console.log("fizzbuzz")
+        }else {
+            console.log("fizz")
+        }
+        
+    } else{
+        if(i%5 ===0){
+            console.log("buzz")
+        } else {
+            console.log(i)
+        }
+            
+    }
+
+}
