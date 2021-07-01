444033f3f883b96269dd9bc10bc176c65a08bd47
kawaabtamayu <2195032@stu.hus.ac.jp>
2021-07-01 14:08:38 +0900

    tukurikake

---
+++ kadai1.js
@@ -0,0 +1,27 @@
+
+console.log("99 bottles of beer on the wall, 99 bottles of beer.")
+console.log("Take one down and pass it around, 98 bottles of beer on the wall.")
+
+console.log("98 bottles of beer on the wall, 98 bottles of beer.")
+console.log("Take one down and pass it around, 97 bottles of beer on the wall.")
+
+console.log("中略")
+
+console.log("1 bottle of beer on the wall, 1 bottle of beer.")
+console.log("Take one down and pass it around, no more bottles of beer on the wall.")
+
+console.log("No more bottles of beer on the wall, no more bottles of beer.")
+console.log("Go to the store and buy some more, 99 bottles of beer on the wall.")
+console.log("99 bottles of beer on the wall, 99 bottles of beer.")
+console.log("Take one down and pass it around, 98 bottles of beer on the wall.")
+
+console.log("98 bottles of beer on the wall, 98 bottles of beer.")
+console.log("Take one down and pass it around, 97 bottles of beer on the wall.")
+
+console.log("中略")
+
+console.log("1 bottle of beer on the wall, 1 bottle of beer.")
+console.log("Take one down and pass it around, no more bottles of beer on the wall.")
+
+console.log("No more bottles of beer on the wall, no more bottles of beer.")
+console.log("Go to the store and buy some more, 99 bottles of beer on the wall.")
