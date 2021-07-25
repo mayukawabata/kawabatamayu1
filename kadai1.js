1c1cd680c8a46a908b4a7feac1dad2185f7673c2
kawaabtamayu <2195032@stu.hus.ac.jp>
2021-07-25 14:07:22 +0900

    修正しました7

--- kadai1.js
+++ kadai1.js
@@ -1,27 +1,15 @@
-
-console.log("99 bottles of beer on the wall, 99 bottles of beer.")
-console.log("Take one down and pass it around, 98 bottles of beer on the wall.")
-
-console.log("98 bottles of beer on the wall, 98 bottles of beer.")
-console.log("Take one down and pass it around, 97 bottles of beer on the wall.")
-
-console.log("中略")
-
-console.log("1 bottle of beer on the wall, 1 bottle of beer.")
+let i;
+for(i=99;0<i;i--){
+   if(i==1){
+        console.log("1 bottle of beer on the wall, 1 bottle of beer.");
+    }else{ 
+}
+     console.log(i+"bottles of beer on the wall, "+(i)+" bottles of beer.");
+     console.log("Take one down and pass it around, "+(i-1)+" bottles of beer on the wall.")
+     console.log("")
+    }
+    
 console.log("Take one down and pass it around, no more bottles of beer on the wall.")
-
-console.log("No more bottles of beer on the wall, no more bottles of beer.")
-console.log("Go to the store and buy some more, 99 bottles of beer on the wall.")
-console.log("99 bottles of beer on the wall, 99 bottles of beer.")
-console.log("Take one down and pass it around, 98 bottles of beer on the wall.")
-
-console.log("98 bottles of beer on the wall, 98 bottles of beer.")
-console.log("Take one down and pass it around, 97 bottles of beer on the wall.")
-
-console.log("中略")
-
-console.log("1 bottle of beer on the wall, 1 bottle of beer.")
-console.log("Take one down and pass it around, no more bottles of beer on the wall.")
-
+console.log("")
 console.log("No more bottles of beer on the wall, no more bottles of beer.")
 console.log("Go to the store and buy some more, 99 bottles of beer on the wall.")
